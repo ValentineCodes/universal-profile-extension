63dee66176931b36a5f82bafc301e367e874ada7
Valentine Orga <valentineorga@gmail.com>
2024-08-25 18:04:42 +0100

    docs: instructions to set up connect button

--- extension/packages/nextjs/app/universalprofile/page.tsx
+++ extension/packages/nextjs/app/universalprofile/page.tsx
@@ -19,39 +19,32 @@ const Home: NextPage = () => {
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
+        <div className="flex flex-col px-5 items-center">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">Scaffold-ETH 2 with Universal Profiles</span>
           </h1>
 
-          <div className="navbar-end flex-grow mr-4">
+          <div className="flex-grow mr-4 w-[300px]">
             <UPRainbowKitCustomConnectButton />
           </div>
+          <p className="text-center text-lg">
+            Replace the RainbowKit connect button in{" "}
+            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+              packages/nextjs/components/Header.tsx
+            </code>{" "}
+            with{" "}
+            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+              packages/nextjs/components/scaffold-eth/UPRainbowKitCustomConnectButton
+            </code>
+          </p>
 
           <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
-            <p className="my-2 font-medium">Connected Address:</p>
+            <p className="my-4 font-medium">Connected Address:</p>
             <UniversalProfileAddress address={connectedAddress} />
           </div>
 
           {isLuksoConnection() && <UniversalProfile />}
-
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
         </div>
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
